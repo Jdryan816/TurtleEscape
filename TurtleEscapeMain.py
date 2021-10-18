e4bc3f38e7d8751355ad19d0428c6adc5639e661
John Ryan <johnryan22@js-student.org>
2021-10-18 09:35:25 -0700

    initial commit

---
+++ TurtleEscapeMain.py
@@ -0,0 +1,15 @@
+import turtle
+
+maze = turtle.Turtle()
+
+maze.speed(0)
+maze.pensize(3)
+
+for index in range(30):
+    maze.fd(20 + 20*index)
+    maze.left(90)
+
+
+wn = turtle.Screen()
+wn.listen()
+wn.mainloop()
